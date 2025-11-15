256ce53a7b0f88b4391e89de198077d557d6417f
Prasanth Jayakumar <prasanth@mallow-tech.com>
2025-11-15 09:50:36 +0530

    Implement user authentication with Laravel UI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'login');
+
+Auth::routes();
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
